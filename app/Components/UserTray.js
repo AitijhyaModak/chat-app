ea1c4c85fdb608083b8ea8895b00a434825742a7
AitijhyaModak <aitijhyasps1@gmail.com>
2024-07-02 18:52:44 +0530

    fixed logout bug

--- app/Components/UserTray.js
+++ app/Components/UserTray.js
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react";
 export default function UserTray() {
   return (
     <div
-      onClick={() => signOut()}
+      onClick={() => signOut({ callbackUrl: "https://am-chatapp.netlify.app" })}
       className="cursor-pointer mt-3 px-10 py-2 bg-red-500 rounded-lg active:bg-red-700"
     >
       <button>LOGOUT</button>
